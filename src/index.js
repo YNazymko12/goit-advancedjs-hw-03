eaa3972f3193e00916fa00df66059a723806e2bb
YNazymko12 <yulychka12@gmail.com>
2023-11-27 11:12:10 +0100

    Update code

--- src/index.js
+++ src/index.js
@@ -10,17 +10,14 @@ const elements = {
 };
 
 let isFirstSelect = true;
+let slimSelect;
 
-elements.loader.classList.remove('is-hidden');
+// elements.loader.classList.remove('is-hidden');
+// elements.select.styles.display = 'none';
+// elements.select.classList.add('is-hidden');
+// elements.slimInput.classList.add('is-hidden');
 
 elements.select.addEventListener('change', handleBreedSelection);
-
-const slimSelect = new SlimSelect({
-  select: '#single',
-  settings: {
-    placeholderText: 'Search breeds',
-  },
-});
 
 const errorMessage = {
   title: 'Error',
@@ -29,16 +26,26 @@ const errorMessage = {
   color: 'red',
 };
 
-
 fetchBreeds()
   .then(breeds => {
     if (!breeds) {
-        elements.select.classList.add('is-hidden');
-        elements.loader.classList.add('is-hidden');
+      elements.loader.classList.add('is-hidden');
+      elements.select.classList.add('is-hidden');
       throw new Error();
     }
     const data = breeds.map(({ id, name }) => ({ text: name, value: id }));
-    slimSelect.setData([{placeholder: true, text: ''}, ...data])
+
+    slimSelect = new SlimSelect({
+      select: '#single',
+      settings: {
+        placeholderText: 'Search breeds',
+        onChange: info => {
+          handleSlimSelectChange(info);
+        },
+      },
+    });
+
+    slimSelect.setData([{ placeholder: true, text: '' }, ...data]);
 
     elements.loader.classList.add('is-hidden');
     elements.select.classList.remove('is-hidden');
@@ -50,41 +57,57 @@ fetchBreeds()
     iziToast.show(errorMessage);
   });
 
- function handleBreedSelection(event) {
-    event.preventDefault();
+function handleBreedSelection(event) {
+  event.preventDefault();
 
-    elements.loader.classList.remove('is-hidden');
-    elements.catInfoBox.innerHTML = '';
+  elements.loader.classList.remove('is-hidden');
+  elements.catInfoBox.innerHTML = '';
 
-    if (isFirstSelect) {
-        isFirstSelect = false;
-        return;
-      }
-    
-
-    const { value } = event.currentTarget;
-
-    fetchCatByBreed(value)
-      .then(data => {
-        if (!data) {
-          elements.loader.classList.add('is-hidden');
-          throw new Error();
-        }
-        elements.catInfoBox.insertAdjacentHTML('beforeend', createBreedCardMarkup(data));
-        elements.catInfoBox.classList.remove('is-hidden');
-        elements.loader.classList.add('is-hidden');
-      })
-      .catch(error => {
-        console.log(error.message);
-        elements.loader.classList.add('is-hidden');
-        iziToast.show(errorMessage);
-      });
- }
-
- function createBreedCardMarkup(data) {
-    const { url } = data[0];
-    const { description, name, temperament } = data[0].breeds[0];
-  
-    const markup = `<img src="${url}" alt="${name} cat" width="400" height="auto"/><div class="breed-text-info"><h1>${name}</h1><p class="description">${description}</p><p><strong>Temperament:</strong> ${temperament}</p></div>`;
-    return markup;
+  if (isFirstSelect) {
+    isFirstSelect = false;
+    return;
   }
+
+  const { value } = event.currentTarget;
+
+  fetchCatByBreed(value)
+    .then(data => {
+      if (!data) {
+        elements.loader.classList.add('is-hidden');
+        throw new Error();
+      }
+      elements.catInfoBox.insertAdjacentHTML(
+        'beforeend',
+        createBreedCardMarkup(data)
+      );
+      elements.catInfoBox.classList.remove('is-hidden');
+      elements.loader.classList.add('is-hidden');
+    })
+    .catch(error => {
+      console.log(error.message);
+      elements.loader.classList.add('is-hidden');
+      iziToast.show(errorMessage);
+    });
+}
+
+function createBreedCardMarkup(data) {
+  const { url } = data[0];
+  const { description, name, temperament } = data[0].breeds[0];
+
+  const markup = `<img src="${url}" alt="${name} cat" width="400" height="auto"/><div class="breed-text-info"><h1>${name}</h1><p class="description">${description}</p><p><strong>Temperament:</strong> ${temperament}</p></div>`;
+  return markup;
+}
+
+function handleSlimSelectChange(info) {
+  const selectedValue = info.value();
+
+  const selectedElement = slimSelect.data.data.find(
+    item => item.value === selectedValue
+  );
+
+  if (selectedElement) {
+    const selectedDOMElement = selectedElement.element;
+
+    selectedDOMElement.classList.add('is-hidden');
+  }
+}
